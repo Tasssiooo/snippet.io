f52fb4d5c273ef45252c200062ec089a378a57a9
Tasssiooo <tassioribeirofontes@gmail.com>
2024-05-21 17:21:20 -0300

    Text color fixed in light mode

--- app/components/layout/footer/index.tsx
+++ app/components/layout/footer/index.tsx
@@ -24,6 +24,7 @@ export default function Footer() {
         maxW="xl"
         mx="auto"
         alignItems="center"
+        color="primary.foreground"
       >
         <Flex
           flexDir="row"
